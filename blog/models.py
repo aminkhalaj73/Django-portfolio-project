2a99c094cbecc793ac12521de3db9d253a1f3320
aminkhalaj73 <mohammad.a.khalaj@gmail.com>
2019-01-15 15:00:41 +0330

    project modified views and a html file added 1/15/2019

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,16 @@ class blog(models.Model) :
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self) :
+        import re
+        summary = self.body
+        return " ".join(re.findall('^((?:\S+\s+){10}\S+).' , summary))
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime(' %b %e %Y ')
+        
+        
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from .models import blog
 
-# Create your views here.
+def allblogs(request):
+    allblogs = blog.objects
+    return render(request , 'blog/allblogs.html' , {'blogs' : allblogs})
+
+def detail(request , blog_id):
+    detailblog = get_object_or_404(blog , pk = blog_id)
+    return render(request , 'blog/detail.html' , {'details' : detailblog})
